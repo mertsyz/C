b5df6bf759a59ed9f13ef9c02499235d71a4bad3
Mert <91729406+mertsyz@users.noreply.github.com>
2023-08-03 09:44:54 +0300

    Add files via upload

---
+++ Hata_yonetimi/Hata_yonetimi/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hata_yonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // hata yönetimi
+
+            try
+            {
+                int s1, s2, sonuc;
+                Console.Write("Sayı1: ");
+                s1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Sayı2: ");
+                s2 = Convert.ToInt32(Console.ReadLine());
+                sonuc = s1 * s2;
+                Console.Write("İşlem Sonucu: " + sonuc);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Hata");
+            }
+            finally
+            {
+                Console.WriteLine("Burası Çalıştı");
+            }
+            catch(FormatException)
+            {
+
+                Console.WriteLine("Sadece sayısal değer giriniz.");
+                
+            }
+            catch(OverflowException)
+            {
+                Console.WriteLine("Lütfen aralağın dışında değer girmeyin.");
+            } 
+
+
+
+            Console.Read();
+        }
+    }
+}
